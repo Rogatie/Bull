5926fa9782b11907c45197e1c67843a2f8bffb64
Rogatie <155913873+Rogatie@users.noreply.github.com>
2024-02-05 11:17:47 +0100

    files added
    Optional

---
+++ script.js
@@ -0,0 +1,4 @@
+var start = 'DOBRO UTRO ', name, end = ' !', result;
+name = prompt('KAKVO E VASHETO PURVO IME?');
+result = start + name + end + '\nShtraknete vurkhu OK, za da produlzhite da kandidat°stvate za onlain kredit';
+alert(result);
